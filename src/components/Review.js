8c3822cb45d2a1682c7d15d541ca70e91a3f99b1
dayapule-venkatesh <dayapulevenkatesh@gmail.com>
2026-07-10 18:12:35 +0530

    submit

--- src/components/Review.js
+++ src/components/Review.js
@@ -49,10 +49,10 @@ export const Review = (prop) => {
 
   const [count, setCount] = useState(0);
   
-
+let a=`author-${count}`
   return (
     <div className="review" >
-      <p className="author" id="author-id">
+      <p className="author" id={a}>
         author:{ele[count].name}
       </p>
       <p className="job">job:{ele[count].job}</p>
